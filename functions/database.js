2f10e9da3f0f259378bb0b69494fe16754b23efd
Filipe <83041450+filipe1238@users.noreply.github.com>
2024-04-08 18:50:02 -0300

    atualizado repo para socket io

--- functions/database.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-const db = admin.firestore()
-db.settings({ ignoreUndefinedProperties: true })
-
-
-module.exports = db
--- functions/index.js
+++
@@ -1,4 +0,0 @@
-const functions = require('firebase-functions');
-const routes = require('./routes');
-
-exports.api = functions.https.onRequest(routes)
